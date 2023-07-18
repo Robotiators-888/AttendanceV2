f18803b452e345e3e1840803011ab9d8ac703904
Spencer McGrath <Samcgrath2005@gmail.com>
2023-07-18 14:38:56 -0400

    tried importing excel class

--- app/src/main/java/attendancev2/RedisHandler.java
+++ app/src/main/java/attendancev2/RedisHandler.java
@@ -2,8 +2,7 @@ package attendancev2;
 
 import attendancev2.util.ResponceBuilder;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPooled;
+
 
 public class RedisHandler {
     Jedis jedis = new Jedis("localhost", 8888);
@@ -14,21 +13,14 @@ public class RedisHandler {
     }
 
     public boolean forcePut(int db, String id, String name){
-        int pastDB = jedis.getDB();
         jedis.select(db);
-        long pos = jedis.dbSize() + 1;
         jedis.set(id, name);
 
         return true;
     }
 
     public boolean logInOut(String id, boolean isLogin){
-
-        long currentTime;
-        String type = (isLogin)? "LOGIN" : "LOGOUT";
         jedis.select(0);
-        currentTime = System.currentTimeMillis();
-        
         System.out.println(jedis.set(id.toString(), ResponceBuilder.buildDataInput(isLogin, getName(id)).toString()));
         jedis.save();
         
